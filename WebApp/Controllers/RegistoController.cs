f83bf6c8f01bdea2803c58a4378311aae337f850
JoaoSantos497 <210100497@esg.ipsantarem.pt>
2024-10-11 16:25:41 +0100

    Adicionar arquivos de projeto.

---
+++ WebApp/Controllers/RegistoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class RegistoController : Controller
+    {
+        // GET: RegistoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // POST: RegistoController
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(LoginController.Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApp/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+        public decimal Preço { get; set; }
+        public string? Stock { get; set; }
+    }
+}
