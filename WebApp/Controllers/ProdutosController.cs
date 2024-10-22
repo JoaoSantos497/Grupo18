18e10f485c308a46fe5352de2e4cb78672424dd1
JoaoSantos497 <210100497@esg.ipsantarem.pt>
2024-10-22 15:58:55 +0100

    Revert "add boostrap, falta arranjar a pagina inicial para não ficaram sobrepostas com a antiga"
    This reverts commit 976469d18824bf5ad20323049d7ac62965a4073f.

--- WebApp/Controllers/ProdutosController.cs
+++ WebApp/Controllers/ProdutosController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-//using WebApp.Models;
+using WebApp.Models;
 
 
 namespace WebApp.Controllers
@@ -8,14 +8,20 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
+            var produtos = new List<Produto>
+            {
+                new Produto { Id = 1, Nome = "Teclado", Marca = "Razer", Modelo = "Blackwidow", Preço = 220, Stock = "5" },
+                new Produto { Id = 3, Nome = "Rato", Preço = 100 },
+                new Produto { Id = 4, Nome = "Monitor", Preço = 800 }
+            };
 
-            return View();
+            return View(produtos);
         }
 
-        //[Route("[controller]/{id?}")]
-        /*public IActionResult Produto(int id)
+        [Route("[controller]/{id?}")]
+        public IActionResult Produto(int id)
         {
             return View(new Produto { Id = id, Nome = "Teclado", Marca = "Razer", Modelo = "Blackwidow", Preço = 220, Stock = "5" });
-        }*/
+        }
     }
 }
