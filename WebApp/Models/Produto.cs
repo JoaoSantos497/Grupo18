f72343b92ed2c66246abf3d79c1bd2ddf0d8b038
João Santos <210100497@esg.ipsantarem.pt>
2024-10-28 21:16:33 +0000

    updates

---
+++ WebApp/Models/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using WebApp.Models;
+
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        // ID do produto (chave primária)
+        public int Id { get; set; }
+
+        // Nome do produto
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do produto não pode ter mais que 100 caracteres.")]
+        public required string Nome { get; set; }
+
+        // Descrição do produto
+        [Required(ErrorMessage = "A descrição do produto é obrigatória.")]
+        [StringLength(500, ErrorMessage = "A descrição do produto não pode ter mais que 500 caracteres.")]
+        public required string Descricao { get; set; }
+
+        // Preço do produto
+        [Required(ErrorMessage = "O preço do produto é obrigatório.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+        public decimal Preco { get; set; }
+
+        // Quantidade em estoque
+        [Required(ErrorMessage = "A quantidade em estoque é obrigatória.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A quantidade não pode ser negativa.")]
+        public int StockQuantity { get; set; }
+
+        // URL da imagem do produto
+        [Url(ErrorMessage = "A URL da imagem do produto não é válida.")]
+        public required string ImageUrl { get; set; }
+
+        // Data de criação do produto
+        public DateTime CreatedDate { get; set; }
+
+        // Campo para verificar se o produto está ativo
+        public bool IsActive { get; set; } // Para ativar/desativar produtos
+    }
+}
---
+++ WebApp/Models/Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Users
+    {
+        // ID do utilizador (chave primária)
+        public int Id { get; set; }
+
+        // Nome do utilizador
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome não pode ter mais que 100 caracteres.")]
+        public required string Username { get; set; }
+
+        // Email do utilizador
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email não é válido.")]
+        public required string Email { get; set; }
+
+        // Senha do utilizador
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [StringLength(100, ErrorMessage = "A senha deve ter pelo menos {2} caracteres.", MinimumLength = 6)]
+        public required string Password { get; set; }
+
+        // Data de registo
+        public DateTime RegisteredDate { get; set; }
+
+        // Outros campos que você pode querer adicionar
+        public bool IsActive { get; set; }  // Para ativar/desativar utilizadores
+        public required string Role { get; set; }     // Para definir papéis de utilizador (Admin, Usuário, etc.)
+    }
+}
