f3a2160e3a1359f66d3450da36cea20f2b96922d
JoaoSantos497 <210100497@esg.ipsantarem.pt>
2024-10-29 14:30:15 +0000

    updates

---
+++ WebApp/Models/WishlistItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models
+{
+ 
+        public class WishlistItem
+        {
+            public int Id { get; set; }
+            public required string Nome { get; set; }
+            public required string Marca { get; set; }
+            public required string Modelo { get; set; }
+            public required string Descricao { get; set; }
+            public decimal Preco { get; set; }
+        }
+}
+ 
